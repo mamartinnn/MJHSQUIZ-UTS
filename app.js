d8ca5c6c860bcf38066c48898dae4a96e1d6016a
Backburner <martinferdinandharno@gmail.com>
2025-04-12 13:14:59 +0700

    import methode overdrive , change some code lines

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express'); 
 const app = express(); 
+const methodOverride = require("method-override");
 const indexRouter = require("./routes/index");
 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,15 @@ router.get("/", (req, res) => {
     res.render("home", { title: "Home" }); 
 }); 
 
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    
+    // Placeholder login logic
+    if (username === "admin" && password === "admin") {
+        res.send("Login successful!");
+    } else {
+        res.send("Invalid credentials");
+    }
+});
+
 module.exports = router;
