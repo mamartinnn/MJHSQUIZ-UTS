218326a0a1ecbcf2d2cdf770db3b9eafc88d1dc1
Natsoons <nicholassalim2006@gmail.com>
2025-04-24 17:07:34 +0700

    add Styling

--- app.js
+++ app.js
@@ -1,56 +1,66 @@
-const express = require('express'); 
-  const app = express(); 
-  const methodOverride = require("method-override");
-  const session = require("express-session"); 
-  const flash = require("connect-flash");     
-  const indexRouter = require("./routes/index");
-  const mongoose = require("mongoose");
-  const User = require("./db/models/user"); 
-  app.use(methodOverride("_method"));
-  const main = async () => {
-    await mongoose.connect('mongodb://localhost:27017/Login-tut')
-  };
-  
+const express = require('express');
+const path = require('path'); // Add this line
+const app = express();
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+const User = require("./db/models/user");
 
-  main()
+app.use(methodOverride("_method"));
+
+// Connect to MongoDB
+const main = async () => {
+  await mongoose.connect('mongodb://localhost:27017/Login-tut');
+};
+
+main()
   .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log("err"));
+  .catch((err) => console.log("Error connecting to DB:", err));
 
-  app.use(express.urlencoded({ extended: true })); 
-  app.use(express.json());
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Set view engine and views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-  app.set("view engine", "ejs");
+// Serve static files from 'public' directory (create this directory if it doesn't exist)
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Session configuration
+app.use(session({
+  secret: "supersecretkey",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { httpOnly: true }
+}));
 
-  app.use(session({
-    secret: "supersecretkey", // or use process.env.ACCESS_TOKEN_SECRET
-    resave: false,
-    saveUninitialized: true,
-    cookie: { httpOnly: true }
-  }));
+app.use(flash());
 
+// User authentication middleware
+app.use(async (req, res, next) => {
+  if (req.session.user_id) {
+    const user = await User.findById(req.session.user_id);
+    res.locals.user = user;
+    req.user = user;
+  } else {
+    res.locals.user = null;
+  }
+  next();
+});
 
-  app.use(flash());
+// Flash messages middleware
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
-  app.use(async (req, res, next) => {
-    if (req.session.user_id) {
-      const user = await User.findById(req.session.user_id);
-      res.locals.user = user; // Make the user available in all views
-      req.user = user;
-    } else {
-      res.locals.user = null; // No user logged in
-    }
-    next();
-  });
+// Routes
+app.use("/", indexRouter);
 
-  app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    next();
-  });
-
-  app.use("/", indexRouter);
-
-  // Start the server
-  app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// Start the server
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
